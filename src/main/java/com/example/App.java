e2590d35157ddfd3572a11ed5dff63a20924806e
xing <xinghua24@gmail.com>
2018-12-22 11:47:18 -0500

    format file

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,12 +2,12 @@ package com.example;
 
 public class App {
     public long fibonacci(int n) {
-        if(n == 0 ) {
+        if (n == 0) {
             return 0;
-        } else if ( n == 1 ) {
+        } else if (n == 1) {
             return 1;
         } else {
-            return fibonacci(n-1) + fibonacci(n-2);
+            return fibonacci(n - 1) + fibonacci(n - 2);
         }
     }
 }
